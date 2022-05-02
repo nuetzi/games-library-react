dbcea2d0da6484435cafc8d7efd230e293d54c93
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-05-02 14:57:28 -0400

    Making surenapp works on Heroku

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,25 @@ import { useState, useEffect } from "react";
 import { getTopRated } from "../services/igdbAPI";
 
 export default function Home() {
-    // const [games, setGames] = useState([]);
+    const [games, setGames] = useState([]);
 
-    // useEffect(() => {
-    //     getTopRated()
-    //     .then(res => setGames(res.data))
-    // }, []);
+    useEffect(() => {
+        getTopRated()
+        .then(res => setGames(res.data))
+    }, []);
 
-    // return(
-    //     <div>
-    //         <h2>Top 50</h2>
-    //         <div className="gamesContainer">
-    //             {games.map((each, i) => {
-    //                 return(
-    //                     <div className="gamesDisplay" key={i}>
-    //                     <a href={each.url}>{each.name}</a>
-    //                     </div>
-    //                 );
-    //             })}
-    //         </div>
-    //     </div>
-    // );
+    return(
+        <div>
+            <h2>Top 50</h2>
+            {/* <div className="gamesContainer">
+                {games.map((each, i) => {
+                    return(
+                        <div className="gamesDisplay" key={i}>
+                        <a href={each.url}>{each.name}</a>
+                        </div>
+                    );
+                })}
+            </div> */}
+        </div>
+    );
 }
--- src/services/igdbAPI.js
+++ src/services/igdbAPI.js
@@ -1,43 +1,28 @@
 import axios from "axios";
-const IGDB_URL = "https://d3dta3yl9f.execute-api.us-west-2.amazonaws.com/production/v4/";
-
-export function getTopRated() {
-    const axios = require('axios');
-    let data = 'fields aggregated_rating,cover.url,first_release_date,genres.name,involved_companies.company.name,name,platforms.name,rating,storyline,summary,total_rating,url; where aggregated_rating>90 & aggregated_rating_count>10; sort aggregated_rating desc; limit 50;';
-
-    let config = {
-        method: 'post',
-        url: 'https://d3dta3yl9f.execute-api.us-west-2.amazonaws.com/production/v4/games',
-        headers: { 
-            'Content-Type': 'text/plain'
-        },
-        data : data
-    };
-
-    axios(config)
-    .then((response) => {
-        console.log(JSON.stringify(response.data));
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-};
+const IGDB_URL = "https://pb1et55wz5.execute-api.us-west-2.amazonaws.com/production/v4/";
 
 // export function getTopRated() {
-//     let data = "fields aggregated_rating,cover.url,first_release_date,genres.name,involved_companies.company.name,name,platforms.name,rating,storyline,summary,total_rating,url; where aggregated_rating>90 & aggregated_rating_count>10; sort aggregated_rating desc; limit 50;";
+//     const axios = require('axios');
+//     let data = 'fields aggregated_rating,cover.url,first_release_date,genres.name,involved_companies.company.name,name,platforms.name,rating,storyline,summary,total_rating,url; where total_rating>85 & platforms=(19); sort total_rating desc; limit 50;';
 
 //     let config = {
-//         method: "post",
-//         url: `${IGDB_URL}games`,
+//         method: 'post',
+//         url: 'https://pb1et55wz5.execute-api.us-west-2.amazonaws.com/production/v4/games',
 //         headers: { 
-//             "Content-Type": "text/plain"
+//             'Content-Type': 'text/plain'
 //         },
-//         data: data
+//         data : data
 //     };
 
-//     const response = axios(config);
-//     return response;
+//     axios(config)
+//     .then((response) => {
+//         return(response.data);
+//     })
+//     .catch((error) => {
+//     console.log(error);
+//     });
 // };
+
 
 // export function getPlatforms() {
 //     return axios({
